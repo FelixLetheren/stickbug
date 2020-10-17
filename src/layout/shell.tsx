9462eaefae033c11aac498fbb585c8d0d393d079
Felix Letheren <felix@rocketmakers.com>
2020-10-17 21:26:22 +0100

    fix helmet and redirect bug.

--- src/layout/shell.tsx
+++ src/layout/shell.tsx
@@ -1,24 +1,11 @@
 import * as React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import { Helmet } from 'react-helmet';
-import { HeaderQuery } from '../../types/graphql-types';
 
 import './shell.scss';
 
 const Shell: React.FunctionComponent = ({ children }) => {
   // Codegen will append 'query' onto any query name so just use the root word as below.
   // Write your gql query, save the file and your typings will be generated automatically.
-  const data: HeaderQuery = useStaticQuery(graphql`
-    query Header {
-      site {
-        siteMetadata {
-          siteName
-        }
-      }
-    }
-  `);
-
-  const siteName = data.site.siteMetadata.siteName;
 
   return (
     <>
@@ -37,7 +24,7 @@ const Shell: React.FunctionComponent = ({ children }) => {
         <meta name="theme-color" content="#ffffff" />
       </Helmet>
       <header>
-        <div className="fixed-width">{siteName}</div>
+        <div className="fixed-width">get stick bugged lol</div>
       </header>
       <main>{children}</main>
       <footer>© {new Date().getFullYear()} Felix Letheren</footer>
